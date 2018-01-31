d7c8df24415d160fba439190dff98d34f28fd339
Hector Palomares <hector.palomares@laureate.net>
2018-01-30 18:57:32 -0600

    Add getChange(). Passing all 4 tests. JSDoc

--- vending-machine-tdd/functions.js
+++ vending-machine-tdd/functions.js
@@ -1,3 +1,28 @@
-function getChange(totalPayable, cashPaid) {
-	return [];
+'use strict';
+
+/**
+ * getChange accepts two parameters (price and paid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} price the integer amount (in pennies) to be paid
+ * @param {number} paid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // returns [50, 20, 10, 5]
+ */
+function getChange(price, paid) {
+
+	const COINS = [200, 100, 50, 20, 10, 5, 2, 1];
+
+	let totalDifference = paid - price;
+	let change = [];
+
+	COINS.forEach(function(coin) {
+		// Keep adding the current coin until the coin is larger than the difference
+		while(totalDifference >= coin){
+			change.push(coin);
+			totalDifference = totalDifference - coin;
+		}
+	});
+
+	return change;
 }
--- vending-machine-tdd/tests.js
+++ vending-machine-tdd/tests.js
@@ -1,6 +1,24 @@
  // This is what a simple unit test looks like:
-test('getChange(1, 1) should equal [] - and empty array', function(assert) {
-	let result = getChange(1, 1); // no change/coins just and empty array
-	let expected = [];
+test('getChange(215, 300) should equal [50, 20, 10, 5]', function(assert) {
+	let result = getChange(215, 300); // no change/coins just and empty array
+	let expected = [50, 20, 10, 5];
+	assert.deepEqual(result, expected);
+});
+
+test('getChange(468, 600) should equal [100, 20, 10, 2]', function(assert) {
+	let result = getChange(468, 600); // no change/coins just and empty array
+	let expected = [100, 20, 10, 2];
+	assert.deepEqual(result, expected);
+});
+
+test('getChange(112, 150) should equal [20, 10, 5, 2, 1]', function(assert) {
+	let result = getChange(112, 150); // no change/coins just and empty array
+	let expected = [20, 10, 5, 2, 1];
+	assert.deepEqual(result, expected);
+});
+
+test('getChange(2, 5) should equal [2, 1]', function(assert) {
+	let result = getChange(2, 5); // no change/coins just and empty array
+	let expected = [2, 1];
 	assert.deepEqual(result, expected);
 });
